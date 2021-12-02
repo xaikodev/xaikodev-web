54f1543c41fa50c61b90b4bbc2fce39052153335
XaikoDev <xaikodev@gmail.com>
2021-12-02 15:31:42 +0200

    Adding projects page template

---
+++ pages/projects.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { Main } from '../components/Main'
+
+
+export const Projects:NextPage = (props) => {
+    return (
+        <Main>
+            Under Construction
+        </Main>
+    )
+}
