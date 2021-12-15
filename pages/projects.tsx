35ce8a7fd889a697a4863d4ff4b6d25e927775a0
XaikoDev <xaikodev@gmail.com>
2021-12-15 22:38:45 +0200

    fixing 2nd build error

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,11 +2,12 @@ import { NextPage } from 'next'
 import React from 'react'
 import { Main } from '../components/Main'
 
-
-export const Projects:NextPage = (props) => {
+const Projects:NextPage = (props) => {
     return (
         <Main>
             Under Construction
         </Main>
     )
 }
+
+export default Projects
