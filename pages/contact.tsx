95826b2098c7b0e8f538169cc3a095df052079d9
XaikoDev <xaikodev@gmail.com>
2021-12-18 15:49:24 +0200

    contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Page } from "src/components/common/Page";
+
+const Contact: NextPage = () => {
+  return (
+ <Page title="Contact">
+   <h1>Contact Page Under Contruction</h1>
+ </Page>
+  );
+};
+
+export default Contact;
