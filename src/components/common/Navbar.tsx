a5a9b2f22bcce32fed3488315779eb18b28a260f
XaikoDev <xaikodev@gmail.com>
2021-12-27 15:03:09 +0200

    navbar implementation

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,22 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Link from "next/link";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/common/navbar.module.css";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Navbar: FC = (props) => {
+  const [opened, setOpened] = useState(true);
+
+  const toggle = () => {
+    setOpened((op) => !op);
+  };
+
   return (
-    <nav className={styles.links}>
-      <Link href="/">
-        <a className={styles.link}>Home</a>
-      </Link>
-      <Link href="/bio">
-        <a className={styles.link}>Bio</a>
-      </Link>
-      <Link href="/projects">
-        <a className={styles.link}>Projects</a>
-      </Link>
-      <Link href="/contact">
-        <a className={styles.link}>Contact</a>
-      </Link>
+    <nav className={styles.navbar}>
+      <div className={opened ? styles.nav : styles.navClosed}>
+        <Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link>
+        <Link href="/bio">
+          <a className={styles.link}>Bio</a>
+        </Link>
+        <Link href="/projects">
+          <a className={styles.link}>Projects</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.link}>Contact</a>
+        </Link>
+      </div>
+      <HamburgerIcon className={opened ? styles.burger: styles.burgerClosed} onClick={toggle} />
     </nav>
   );
 };
