d6c08cf506f1f92863022fb413808921b5cf1ca4
XaikoDev <xaikodev@gmail.com>
2021-12-28 19:18:54 +0200

    universum age build error

--- pages/universum.tsx
+++ pages/universum.tsx
@@ -1,24 +1,8 @@
-import { Box } from "@chakra-ui/layout";
 import { Game } from "src/components/Universum/Game/Game";
 import type { NextPage } from "next";
-import Head from "next/head";
-import { Footer } from "../src/components/common/Footer";
-import { Main } from "../src/components/common/Main";
-import styles from "../../styles/Home.module.css";
 
 const Universum: NextPage = () => {
-  return (
-    <Box className={styles.container}>
-      <Head>
-        <title>Universum</title>
-        <meta name="description" content="Universum" />
-      </Head>
-      <Main>
-        <Game/>
-      </Main>
-    <Footer/>
-    </Box>
-  );
+  return <Game />;
 };
 
 export default Universum;
