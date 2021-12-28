4ecd02ac2035e1510b4f63234d7983c2aed940f0
XaikoDev <xaikodev@gmail.com>
2021-12-28 19:15:38 +0200

    weird build error fix

--- pages/bio.tsx
+++ pages/bio.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { Page } from "src/components/common/Page";
 
 const Bio: NextPage = (props) => {
-  return <h1>Xaiko's Bio Under Construction</h1>;
+  return <h1>Bio Under Construction</h1>;
 };
 
 export default Bio;
