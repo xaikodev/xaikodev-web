fae6e88a09325080ff43f6b9e95135f704ab53dc
Lucian Petri <24479867+LucianPetri@users.noreply.github.com>
2022-01-29 19:30:48 +0200

    added recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Page } from "src/components/common/Page";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <RecoilRoot>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
