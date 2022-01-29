873c38ac829f790a16356f9098949116be9b45e2
Lucian Petri <petri.lucian@outlook.com>
2022-01-29 22:53:15 +0200

    removed recoil root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,13 @@ import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Page } from "src/components/common/Page";
 import { ChakraProvider } from "@chakra-ui/react";
-import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <RecoilRoot>
-        <Page>
-          <Component {...pageProps} />
-        </Page>
-      </RecoilRoot>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
     </ChakraProvider>
   );
 }
